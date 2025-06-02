8269b01954c065535d91105857acd9e419467853
Jean-Philippe Lachance <jplachance@coveo.com>
2025-06-02 14:14:11 -0400

    fix: make the diff readable
    J:DEF-2843

--- sort.py
+++ sort.py
@@ -1,20 +1,26 @@
 import os
-import yaml
+
+from ruamel.yaml import YAML
+
 
 def sort_allow_licenses_in_file(filepath):
-    with open(filepath, 'r') as f:
-        data = yaml.safe_load(f)
+    yaml = YAML()
+    yaml.preserve_quotes = True
+    with open(filepath, "r") as f:
+        data = yaml.load(f)
 
-    if 'allow-licenses' in data and isinstance(data['allow-licenses'], list):
-        data['allow-licenses'] = sorted(data['allow-licenses'], key=str.lower)
+    if "allow-licenses" in data and isinstance(data["allow-licenses"], list):
+        data["allow-licenses"] = sorted(data["allow-licenses"], key=str.lower)
 
-        with open(filepath, 'w') as f:
-            yaml.dump(data, f, sort_keys=False, width=1000)
+        with open(filepath, "w") as f:
+            yaml.dump(data, f)
+
 
 def main():
-    for filename in os.listdir('.'):
-        if filename.endswith('-v2.yml'):
+    for filename in os.listdir("."):
+        if filename.endswith("-v2.yml"):
             sort_allow_licenses_in_file(filename)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
